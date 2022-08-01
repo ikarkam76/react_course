81aa36342290a18f6a0082d450681055e882b814
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-08-01 17:04:17 +0300

    create counter

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
 class Counter extends React.Component {
+    state = {
+    value: 0,
+    }
+
+    handleIncrement = () => {
+        this.setState(prevstate => ({
+            value: prevstate.value + 1,
+        }));
+        console.log("add 1");
+    }
+    handleDescrement = () => {
+        this.setState((prevstate) => ({
+          value: prevstate.value - 1,
+        }));
+        console.log("remove 1");
+    }
     render() {
         return (
             <div>
-                <span>0</span>
-                <button>Add</button>
-                <button>Remove</button>
+                <span>{this.state.value}</span>
+                <button onClick={this.handleIncrement}>Add</button>
+                <button onClick={this.handleDescrement}>Remove</button>
             </div>
         )
     }
