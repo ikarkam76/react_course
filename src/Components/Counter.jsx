3f4a95c83397f9e35032a75312f64c14066edd9e
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-08-01 13:54:33 +0300

    create components

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Counter extends React.Component {
+    render() {
+        return (
+            <div>
+                <span>0</span>
+                <button>Add</button>
+                <button>Remove</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
